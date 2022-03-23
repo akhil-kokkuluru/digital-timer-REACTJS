0cf3bb40450411f8f4dba07ef94546de8cacaf6b
akhil raja rao <akhilr37.gmail.com>
2022-03-23 09:49:48 +0000

    buttons addition half completed

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -12,6 +12,27 @@ class DigitalTimer extends Component {
             <p className="timertext">Timer</p>
           </div>
         </div>
+        <div className="controllersContainer">
+          <div className="playPausecss">
+            <button className="buttonCss" type="button">
+              <img
+                className="playcss"
+                alt="play icon"
+                src="https://assets.ccbp.in/frontend/react-js/play-icon-img.png"
+              />
+              <p className="buttonText">Start</p>
+            </button>
+            <button className="buttonCss" type="button">
+              <img
+                className="playcss"
+                alt="play icon"
+                src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
+              />
+              <p className="buttonText">Reset</p>
+            </button>
+          </div>
+        </div>
+        <p className="timerPhrase">Set Timer limit</p>
       </div>
     )
   }
