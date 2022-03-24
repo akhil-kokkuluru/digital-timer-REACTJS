9ba9b44cc370ef6c74f7ddb6bfbe0bcc90643835
akhil <akhilr37@gmail.com>
2022-03-24 07:04:35 +0000

    UI design completed

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -6,33 +6,45 @@ class DigitalTimer extends Component {
     return (
       <div className="totalBG">
         <h1>Digital Timer</h1>
-        <div className="timerContainer">
-          <div className="timeContainer">
-            <h1 className="timer">25:00</h1>
-            <p className="timertext">Timer</p>
+        <div className="timerControllerContainer">
+          <div className="timerContainer">
+            <div className="timeContainer">
+              <h1 className="timer">25:00</h1>
+              <p className="timertext">Timer</p>
+            </div>
+          </div>
+          <div className="controllersContainer">
+            <div className="playPausecss">
+              <button className="buttonCss" type="button">
+                <img
+                  className="playcss"
+                  alt="play icon"
+                  src="https://assets.ccbp.in/frontend/react-js/play-icon-img.png"
+                />
+                <p className="buttonText">Start</p>
+              </button>
+              <button className="buttonCss" type="button">
+                <img
+                  className="playcss"
+                  alt="play icon"
+                  src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
+                />
+                <p className="buttonText">Reset</p>
+              </button>
+            </div>
+            <p className="timerPhrase">Set Timer limit</p>
+            <div className="timeAdjustContainer">
+              <button className="plusMinusButton" type="button">
+                +
+              </button>
+
+              <p className="timerValue">25</p>
+              <button className="plusMinusButton" type="button">
+                -
+              </button>
+            </div>
           </div>
         </div>
-        <div className="controllersContainer">
-          <div className="playPausecss">
-            <button className="buttonCss" type="button">
-              <img
-                className="playcss"
-                alt="play icon"
-                src="https://assets.ccbp.in/frontend/react-js/play-icon-img.png"
-              />
-              <p className="buttonText">Start</p>
-            </button>
-            <button className="buttonCss" type="button">
-              <img
-                className="playcss"
-                alt="play icon"
-                src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
-              />
-              <p className="buttonText">Reset</p>
-            </button>
-          </div>
-        </div>
-        <p className="timerPhrase">Set Timer limit</p>
       </div>
     )
   }
